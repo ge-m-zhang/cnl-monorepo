a73d095b2f380c6eb6924906fc767a2fe88b5ae7
gezhang <ge.melody.zhang@gmail.com>
2024-09-23 19:34:37 -0400

    added file path for tailwindcss
    Signed-off-by: gezhang <ge.melody.zhang@gmail.com>

--- cnl-fe/tailwind.config.js
+++ cnl-fe/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+
+    './src/**/*.{html,js,jsx,ts,tsx,vue}',
+
+  ],
   theme: {
     extend: {},
   },
