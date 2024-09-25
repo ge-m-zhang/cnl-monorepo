06cc39231bcdfcde09c0c81ecf7b938ede7ac4a0
gezhang <ge.melody.zhang@gmail.com>
2024-09-25 18:56:11 -0400

    simple route
    Signed-off-by: gezhang <ge.melody.zhang@gmail.com>

--- cnl-fe/src/App.tsx
+++ cnl-fe/src/App.tsx
@@ -1,8 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { LandingPage } from './pages/LandingPage';
+import ProfilePage from './pages/ProfilePage';
+
 
 function App() {
   return (
-    <LandingPage />
+    
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+ 
   );
 }
 
--- cnl-fe/src/pages/ProfilePage.tsx
+++ cnl-fe/src/pages/ProfilePage.tsx
@@ -4,20 +4,20 @@ import { apiClient } from "../api/api";
 
 const ProfilePage: React.FC = () => {
    
-  const { data: profile, error, isLoading } = useQuery({
+  const { data, error, isLoading } = useQuery({
     queryKey: ['getProfile'],
     queryFn: apiClient.getProfile
   });
   
-console.log("profile: ",profile);
+console.log("profile: ",data);
+
     if (isLoading) return <div>Loading...</div>;
     if (error) return <div>Error fetching profile data</div>;
   
     return (
       <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-        <h1 className="text-4xl font-bold mb-8">Welcome, {profile.firstName}</h1>
-        <p>Email: {profile.email}</p>
-        <img src={profile.picture} alt="Profile" className="rounded-full" />
+        <h1 className="text-4xl font-bold mb-8">Welcome, {data.firstName}</h1>
+        <p> {data.profile?.firstName}</p>
       </div>
     );
   };
