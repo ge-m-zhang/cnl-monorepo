fdadd686c945d87803c03ec7a67a7c1456b94969
gezhang <ge.melody.zhang@gmail.com>
2024-12-17 17:02:12 -0500

    save all lint fixes - BE

--- cnl/cnl-be/src/configuration/configuration.modules.ts
+++ cnl/cnl-be/src/configuration/configuration.modules.ts
@@ -1,6 +1,7 @@
 import { Global, Module } from '@nestjs/common';
-import { ConfigurationService } from './configuration.service';
 import { ConfigModule } from '@nestjs/config';
+
+import { ConfigurationService } from './configuration.service';
 
 @Global()
 @Module({
